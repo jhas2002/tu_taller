508ee73f8ae32035f1eb641e5493eb2119c5ff7b
Jhasmani Martinez <jhas20010520@gmail.com>
2023-08-27 00:06:36 -0400

    first commit

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Inicio</title>
+	<link rel="stylesheet" href="<?php echo base_url('sources/css/bootstrap.css') ?>">
+	<script src="<?php echo base_url('sources/js/bootstrap.bundle.min.js') ?>"></script>
+</head>
+<body>
+	<div class="row">
+		<div class="col-md-6">
+			
+		</div>
+		<div class="col-md-6"> 
+			<h1 class="">HOLA MUNDO</h1>
+		</div>
+	</div>
+    
+</body>
+</html>
